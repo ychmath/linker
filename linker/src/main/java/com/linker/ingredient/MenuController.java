83adddbad7a382850ea32e9e74e19ea284c36980
user <pumpkinyeah3@gmail.com>
2023-05-11 16:26:28 +0900

    2023/05/11 작업 내용
    - 달력 객체 만들기: 날짜 클릭 시 반응, 버튼으로 날짜 찾기 가능
    - 공휴일 기능을 활용하여 hashmap 객체로 메뉴 생성 예정 중
    - 오류 상황: arHoliday 객체를 setting에 저장하는 데 문제 발생 중.

---
+++ linker/src/main/java/com/linker/ingredient/MenuController.java
@@ -0,0 +1,12 @@
+package com.linker.ingredient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MenuController {
+	@GetMapping("/menu")
+	public String getPath() {
+		return "/menu/menu";
+	}
+}
